33b85e69030be995f48be5317b392f8cf52f7ffc
XOT <minexo79@gmail.com>
2024-03-06 15:06:12 +0800

    Initial Release

---
+++ App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Lepo.i18n;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
+
+namespace MultiLangTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string langPath = "MultiLangTest.Language.";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Load language file
+            Translator.LoadLanguages(
+                Assembly.GetExecutingAssembly(),
+                new Dictionary<string, string>
+                {
+                    { "en_US", langPath + "en_US.yaml" },
+                    { "zh_TW", langPath + "zh_TW.yaml" }
+                }
+            );
+
+            // Get OS current language And Make UI language Displayed
+            CultureInfo ci = CultureInfo.CurrentUICulture;
+
+            if (ci.Name == "zh-TW")
+                Translator.SetLanguage("zh_TW");
+            else
+                Translator.SetLanguage("en_US");
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MultiLangTest
+Testing Multi Languaue In WPF Using i18n & ModernWpf
+
+# Thirdparty
+- [i18n](https://github.com/lepoco/i18n)
+- [ModernWpf](https://github.com/Kinnara/ModernWpf)
+
+# License
+- MIT License
